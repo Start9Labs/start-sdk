6257f9537525aff303ed5d2a83503e4a404db1d8
BluJ <dragondef@gmail.com>
2023-04-26 09:04:49 -0600

    chore: Add docs to manifest

--- lib/config/index.ts
+++ lib/config/index.ts
@@ -1,5 +1,4 @@
 export * as configBuilder from "./builder";
 
 export { setupConfig } from "./setupConfig";
-export { specToBuilder, specToBuilderFile } from "./specToBuilder";
 export * as dependencies from "./dependencies";
